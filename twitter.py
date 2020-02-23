378ecaa2398b032af7f0f3831daeb338905581e8
anushkagupta-commits <61287662+anushkagupta-commits@users.noreply.github.com>
2020-02-23 13:03:44 +0530

    Create twitter.py

---
+++ twitter.py
@@ -0,0 +1,21 @@
+def bignumber(arr):
+    a,ans=[],''
+    
+    l=len(str(max(arr)))+l
+    
+    for i in arr:
+         temp=str(i)*l
+         a.append((temp[:1:],i))
+         
+    a.sort(reverse==true)
+    
+    for i in a:
+    
+       ans+=str(i[1])
+       
+    return ans
+b=list(map(int,input().split()))
+print(bignumber(b))
+    
+
+        
